f97dbfce9453f948352e09e2de7ed3e0d28a4705
xulei <ashura1110@163.com>
2018-07-28 15:29:00 +0800

    学习MQ-rabbit的一部分代码

---
+++ RabbitMQ-Topic/src/test/java/com/lamber/java/LambdaTest.java
@@ -0,0 +1,29 @@
+package com.lamber.java;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2018/7/16 0016 13:21
+ * @Description:
+ */
+public class LambdaTest {
+
+
+    public static void main(String[] args) {
+
+
+        int a=10;
+        int b=20;
+
+
+
+    }
+
+    public static int add(int x,int y){
+
+        return x+y;
+
+    }
+
+
+
+}

d560ae0eff94b2fd9dd56dcead8b7144a3da671c
xulei <ashura1110@163.com>
2018-08-25 20:31:19 +0800

    增加备注

---
+++ login-Token/src/main/resources/1.sql
@@ -0,0 +1,43 @@
+/*
+SQLyog Ultimate v8.32 
+MySQL - 5.5.27 : Database - datauser
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`datauser` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `datauser`;
+
+/*Table structure for table `t_user` */
+
+DROP TABLE IF EXISTS `t_user`;
+
+CREATE TABLE `t_user` (
+  `t_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `t_name` varchar(30) DEFAULT NULL COMMENT '名称',
+  `t_age` int(10) DEFAULT NULL COMMENT '年龄',
+  `t_address` varchar(100) DEFAULT NULL COMMENT '家庭住址',
+  `t_pwd` varchar(100) CHARACTER SET latin1 DEFAULT NULL COMMENT '用户登录密码',
+  PRIMARY KEY (`t_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_user` */
+
+LOCK TABLES `t_user` WRITE;
+
+insert  into `t_user`(`t_id`,`t_name`,`t_age`,`t_address`,`t_pwd`) values (11,'ddd',34,'ff','vfs'),(10,'sss',12,'ddd','eee'),(9,'akd==',8,'aohoo','5');
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;

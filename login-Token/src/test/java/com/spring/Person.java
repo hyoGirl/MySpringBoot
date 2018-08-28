15760f38ff5556793d17f7070cd7e8e04cd34a2a
xulei <ashura1110@163.com>
2018-08-28 23:12:11 +0800

    增加了自定义比较器

---
+++ login-Token/src/test/java/com/spring/Person.java
@@ -0,0 +1,44 @@
+package com.spring;
+
+public class Person implements Comparable<Person>{
+	
+	private int age;
+	private String name;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public Person() {
+		super();
+	}
+	public Person(int age, String name) {
+		super();
+		this.age = age;
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Person [age=" + age + ", name=" + name + "]";
+	}
+	
+	
+	
+	@Override
+	public int compareTo(Person o) {
+		
+		return this.age-o.getAge();
+	}
+	
+	
+
+}
--- login-Token/src/test/java/com/spring/Test.java
+++ login-Token/src/test/java/com/spring/Test.java
@@ -1,5 +1,11 @@
 package com.spring;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Test {
 
 	
@@ -11,6 +17,28 @@ public class Test {
 		String s1="/aa/login";
 		
 		System.out.println(s1.endsWith(ss));
+		
+		
+		List<Person> list=new ArrayList<Person>();
+		
+		list.add(new Person(1, "CC"));
+		list.add(new Person(2, "Ab"));
+		
+//		Collections.sort(list);
+		Collections.sort(list, new Comparator<Person>() {
+
+			@Override
+			public int compare(Person o1, Person o2) {
+				
+				
+				return o1.getName().compareTo(o2.getName());
+			}
+			
+			
+		});
+		for (Person person : list) {
+			System.out.println(person);
+		}
 		
 	}
 }

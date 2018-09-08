8d33f23a7d81f4c90b0ce134ffd9c5278084e760
xulei <ashura1110@163.com>
2018-09-08 15:54:38 +0800

    学习笔记和SQL文件

---
+++ Mybatis-batch/src/main/resources/1.sql
@@ -0,0 +1,42 @@
+/*
+SQLyog Ultimate v8.32 
+MySQL - 5.5.27 : Database - chunjieboot
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`chunjieboot` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `chunjieboot`;
+
+/*Table structure for table `person` */
+
+DROP TABLE IF EXISTS `person`;
+
+CREATE TABLE `person` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `age` int(11) NOT NULL,
+  `sex` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+
+/*Data for the table `person` */
+
+LOCK TABLES `person` WRITE;
+
+insert  into `person`(`id`,`name`,`age`,`sex`) values (3,'张三',12,1),(4,'李四',15,1),(5,'王五',22,1),(6,'赵柳',22,1),(7,'田七',12,1),(8,'阿狸',12,1),(9,'李3',12,1),(10,'张2',12,1),(11,'李5',12,1),(12,'张4',12,1),(13,'李5',12,1),(14,'李5',12,1),(15,'张4',12,1);
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;

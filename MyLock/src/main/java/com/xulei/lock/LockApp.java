0f7229f29db7e23e784e60267fc7b642c9c266ed
xulei <xulei912@163.com>
2019-07-25 23:52:33 +0800

    增加一个学习例子，关于乐观锁的

---
+++ MyLock/src/main/java/com/xulei/lock/LockApp.java
@@ -0,0 +1,21 @@
+package com.xulei.lock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ashura1110
+ * @ClassName XULEI
+ * @description TODO
+ * @Date 2019/7/25 22:55
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class LockApp {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(LockApp.class,args);
+
+    }
+}
---
+++ MyLock/src/main/java/com/xulei/lock/service/PriceService.java
@@ -0,0 +1,14 @@
+package com.xulei.lock.service;
+
+import com.baomidou.mybatisplus.service.IService;
+import com.xulei.lock.entity.Price;
+
+/**
+ * @author ashura1110
+ * @ClassName XULEI
+ * @description TODO
+ * @Date 2019/7/25 23:13
+ * @Version 1.0
+ */
+public interface PriceService extends IService<Price> {
+}

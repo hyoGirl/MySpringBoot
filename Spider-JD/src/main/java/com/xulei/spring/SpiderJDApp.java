fdc54c8c611a8025e314005e0cd3b46c77ccc774
xulei <xulei912@163.com>
2020-04-19 19:25:01 +0800

    增加配置，同步码云

---
+++ Spider-JD/src/main/java/com/xulei/spring/SpiderJDApp.java
@@ -0,0 +1,19 @@
+package com.xulei.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author XULEI
+ * @ClassName SpiderJDApp
+ * @description TODO
+ * @Date 2020/4/19 18:53
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class SpiderJDApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiderJDApp.class,args);
+    }
+}
